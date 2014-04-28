ae786d9c0e6a2b92468a7128f2a98807f507d38d
James Lynch <james@stormid.com>
2014-04-28 16:36:53 +0100

    Updates styleguide

--- rakefile.rb
+++ rakefile.rb
@@ -1,25 +1,31 @@
 COMPASS_PATH = "content"
 SASS_PATH = "content/sass"
-CSS_PATH = "content/stylesheets/all.css"
+CSS_PATH = "content/css/all.css"
 KSS_TEMPLATE_PATH = "styleguide-template/"
 
-task :default => ['kss:styleguide']
+task :default => ['kss:styleguidequick']
 
 namespace :kss do
 	task :styleguide => ['bundler:update', 'sass:compile'] do 
-		#system "npm install kss"
+		system "npm install kss"
+		Rake::Task['kss:styleguidequick'].invoke()
+		system "git commit -am \"Updates styleguide\""
+		
+	end
+	
+	task :styleguidequick do 
 		system "kss-node #{SASS_PATH}  --template #{KSS_TEMPLATE_PATH} --style #{CSS_PATH}"
 	end
 end
 
 namespace :sass do
 	task :compile do
-		system "compass compile #{COMPASS_PATH} -e production --force"
+		system "compass compile #{COMPASS_PATH} -e production --force --css-dir=css"
 	end
 end
 
 namespace :bundler do
 	task :update do
-		#system "bundle update"
+		system "bundle update"
 	end
 end
