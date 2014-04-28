d2662068b00e0d0aeeafd4d5901a23aba7f70ec0
James Lynch <james@stormid.com>
2014-04-28 16:47:01 +0100

    Updates styleguide

--- rakefile.rb
+++ rakefile.rb
@@ -2,16 +2,20 @@ COMPASS_PATH = "content"
 SASS_PATH = "content/sass"
 CSS_PATH = "content/css/all.css"
 KSS_TEMPLATE_PATH = "styleguide-template/"
+COMMIT_MSG = "Updates styleguide"
 
 task :default => ['kss:styleguidequick']
 
 namespace :kss do
 	task :styleguide => ['bundler:update', 'sass:compile'] do 
-		system "npm install kss"
-		Rake::Task['kss:styleguidequick'].invoke()
-		system "git add -A"
-		system "git commit -am \"Updates styleguide\""
-		
+		commit_data = get_commit_hash_and_date
+		if commit_data[0] != COMMIT_MSG
+			system "npm install kss"
+			Rake::Task['kss:styleguidequick'].invoke()
+			system "git add -A"
+			system "git commit -am \"#{COMMIT_MSG}\""
+			system "git push origin master"
+		end
 	end
 	
 	task :styleguidequick do 
@@ -29,4 +33,16 @@ namespace :bundler do
 	task :update do
 		system "bundle update"
 	end
+end
+
+def get_commit_hash_and_date
+	begin
+		commit = `git log -1 --pretty=format:%H`
+		git_date = `git log -1 --date=iso --pretty=format:%ad`
+		commit_date = DateTime.parse( git_date ).strftime("%Y-%m-%d %H%M%S")
+	rescue
+		commit = "git unavailable"
+	end
+
+	[commit, commit_date]
 end
