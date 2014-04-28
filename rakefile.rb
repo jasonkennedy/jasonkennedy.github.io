6ad5fa9e9df4ec0d0234710a3a94070b99f7f006
James Lynch <james@stormid.com>
2014-04-28 16:05:02 +0100

    Redirecting root index to styleguide

--- rakefile.rb
+++ rakefile.rb
@@ -1,13 +1,13 @@
 COMPASS_PATH = "content"
 SASS_PATH = "content/sass"
 CSS_PATH = "content/stylesheets/all.css"
-KSS_TEMPLATE_PATH = "template/"
+KSS_TEMPLATE_PATH = "styleguide-template/"
 
 task :default => ['kss:styleguide']
 
 namespace :kss do
 	task :styleguide => ['bundler:update', 'sass:compile'] do 
-		system "npm install kss"
+		#system "npm install kss"
 		system "kss-node #{SASS_PATH}  --template #{KSS_TEMPLATE_PATH} --style #{CSS_PATH}"
 	end
 end
@@ -20,6 +20,6 @@ end
 
 namespace :bundler do
 	task :update do
-		system "bundle update"
+		#system "bundle update"
 	end
 end
