f00f266aebc2cedc253455726bcd6bd431fd390c
James Lynch <james@stormid.com>
2014-04-29 16:38:08 +0100

    Adds kss:watch task to rake file

--- Gemfile
+++ Gemfile
@@ -1,3 +1,6 @@
 source "https://rubygems.org"
 
 gem "compass"
+gem 'listen', '~> 2.0'
+require 'rbconfig'
+gem 'wdm', '>= 0.1.0' if RbConfig::CONFIG['target_os'] =~ /mswin|mingw|cygwin/i
--- rakefile.rb
+++ rakefile.rb
@@ -1,3 +1,5 @@
+require 'listen'
+
 COMPASS_PATH = "content"
 SASS_PATH = "content/sass"
 CSS_FILE_PATH = "content/css/all.css"
@@ -13,6 +15,21 @@ namespace :kss do
 	
 	task :styleguide => ['sass:compile'] do 
 		system "kss-node #{SASS_PATH}  --template #{KSS_TEMPLATE_PATH} --style #{CSS_FILE_PATH}"
+	end
+	
+	task :watch do
+		puts "Listening for changes to files in #{SASS_PATH}..."
+		callback = Proc.new do |modified, added, removed|
+			puts "Changes detected!"
+			puts "Modified: #{modified}"
+			puts "Added: #{added}"
+			puts "Removed: #{removed}"
+			puts "Running kss styleguide generation"
+			system "kss-node #{SASS_PATH}  --template #{KSS_TEMPLATE_PATH} --style #{CSS_FILE_PATH}"
+		end
+		listener = Listen.to(SASS_PATH, :force_polling => true, &callback)
+		listener.start # not blocking
+		sleep
 	end
 end
 
